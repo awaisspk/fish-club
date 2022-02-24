ff620841ff6805c41652a72f89db3211a20aa13d
awaisspk <iamawaisiqball@gmail.com>
2022-02-24 16:00:13 +0500

    styles(global): add global styles

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -1,47 +1,47 @@
-import { globalCss } from "@stitches";
+import {globalCss} from '@stitches';
 
 export const globalStyles = globalCss({
-  "@font-face": [
+  '@font-face': [
     {
-      fontFamily: "Poiret One",
-      fontDisplay: "optional",
+      fontFamily: 'Poiret One',
+      fontDisplay: 'optional',
       src: 'url("/fonts/PoiretOne-Regular.woff2") format("woff2")',
     },
     {
-      fontFamily: "Inter",
-      fontStyle: "normal",
-      fontWeight: "100 900",
-      fontDisplay: "optional",
+      fontFamily: 'Inter',
+      fontStyle: 'normal',
+      fontWeight: '100 900',
+      fontDisplay: 'optional',
       src: 'url("/fonts/inter-var-latin.woff2") format("woff2")',
     },
   ],
   html: {
-    font: "100%/1.25 $fonts$primary",
-    boxSizing: "border-box",
+    font: '100%/1.25 $fonts$primary',
+    boxSizing: 'border-box',
   },
 
-  "*, ::before, ::after": {
+  '*, ::before, ::after': {
     margin: 0,
     padding: 0,
-    boxSizing: "inherit",
-    WebkitTapHighlightColor: "transparent",
+    boxSizing: 'inherit',
+    WebkitTapHighlightColor: 'transparent',
   },
 
   a: {
-    textDecoration: "none",
+    textDecoration: 'none',
   },
 
   img: {
-    maxWidth: "100%",
+    maxWidth: '100%',
   },
 
   body: {
     lineHeight: 1.5,
   },
 
-  "h1, h2, h3, h4,li,p": {
-    overflowWrap: "break-word",
-    hyphens: "auto",
-    WebkitHyphens: "auto",
+  'h1, h2, h3, h4,li,p': {
+    overflowWrap: 'break-word',
+    hyphens: 'auto',
+    WebkitHyphens: 'auto',
   },
 });
