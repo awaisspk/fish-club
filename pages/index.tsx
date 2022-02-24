e282e38d16aff9c67bc6a21c3636f44b3f22e06e
awaisspk <iamawaisiqball@gmail.com>
2022-02-24 23:59:46 +0500

    feat(component): add introduction section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,41 @@
 import {Box} from '@components/box';
 import {Header} from '@components/header';
+import {Introduction} from '@components/introduction';
 import {SecondaryNav} from '@components/nav';
-import {H2} from '@components/typography';
+import {H1} from '@components/typography';
 import {PageLayout} from '@src/layouts';
+import {styled} from '@stitches/react';
 import type {NextPage} from 'next';
+
+const Main = styled('main', {
+  background: '#F5F9FC',
+  mx: 'auto',
+});
 
 const Home: NextPage = () => {
   return (
-    <PageLayout>
-      <Header />
-      <main>
-        <Box as="section" css={{minH: '200vh', mt: '100px', mb: '50px'}}>
-          <H2
-            css={{
-              fontSize: 'clamp(2rem, 5vw + 1rem, 4.625rem)',
-              fontWeight: '400',
-              textAlign: 'center',
-            }}
-          >
-            How It Works
-          </H2>
-          <SecondaryNav />
-        </Box>
-      </main>
-    </PageLayout>
+    <Box css={{zIndex: 1}}>
+      <PageLayout>
+        <Header />
+        <Main>
+          <Box as="section" css={{mt: '100px', '@bp4': {mb: '80px'}}}>
+            <H1
+              css={{
+                fontSize: 'clamp(2rem, 5vw + 1rem, 4.625rem)',
+                fontWeight: '400',
+                fontFamily: '$primary',
+                textAlign: 'center',
+                color: '$gray8',
+              }}
+            >
+              How It Works
+            </H1>
+            <SecondaryNav />
+          </Box>
+          <Introduction />
+        </Main>
+      </PageLayout>
+    </Box>
   );
 };
 
--- src/components/introduction/Introduction.tsx
+++ src/components/introduction/Introduction.tsx
@@ -1,7 +1,69 @@
+import {Flex} from '@components/flex';
+import {H2, ListItem, Paragraph} from '@components/typography';
+import {styled} from '@stitches';
+import Image from 'next/image';
+
+const ImageContainer = styled('div', {
+  width: '100%',
+  height: '250px',
+  position: 'relative',
+  overflow: 'hidden',
+  '@bp1': {
+    height: '320px',
+  },
+  '@bp2': {
+    height: '420px',
+  },
+  '@bp3': {
+    height: '520px',
+  },
+  '@bp4': {
+    height: '420px',
+    btlr: '80px',
+    bblr: '80px',
+  },
+});
+
 export const Introduction = () => {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <Flex
+      as="section"
+      gap="9"
+      center
+      direction={{'@initial': 'col', '@bp4': 'row'}}
+    >
+      <ImageContainer css={{'@bp4': {order: 2}, flexGrow: 2.5}}>
+        <Image
+          layout="fill"
+          objectFit="cover"
+          src="/images/pour_wide.webp"
+          alt="pour wide"
+        />
+      </ImageContainer>
+      <Flex
+        gap="5"
+        direction="col"
+        css={{mx: '$10', mt: '$9', flexShrink: 1.8}}
+      >
+        <H2>Introduction</H2>
+        <Paragraph>
+          As the first of its kind, FFC is focused around utility. Our members
+          can use and enjoy our club, converting a typical membership into an
+          asset.
+        </Paragraph>
+        <Flex
+          as="ul"
+          direction="col"
+          gap="3"
+          css={{listStylePosition: 'inside'}}
+        >
+          <ListItem>
+            There are no applications or reoccurring fees each year
+          </ListItem>
+          <ListItem>Members can lease or resell their membership</ListItem>
+          <ListItem>Payment for food and beverage will be in USD</ListItem>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 };
