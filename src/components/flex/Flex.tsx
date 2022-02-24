207ec424193c2c91b64e26c3908015c3957fd6ed
awaisspk <iamawaisiqball@gmail.com>
2022-02-24 15:11:23 +0500

    feat(component): add flex component

---
+++ src/components/flex/Flex.tsx
@@ -0,0 +1,80 @@
+import {styled} from '@stitches';
+import {gap} from './gap';
+
+export const Flex = styled('div', {
+  display: 'flex',
+  variants: {
+    wrap: {
+      wrap: {
+        flexWrap: 'wrap',
+      },
+      noWrap: {
+        flexWrap: 'nowrap',
+      },
+      wrapReverse: {
+        flexWrap: 'wrap-reverse',
+      },
+    },
+    direction: {
+      row: {
+        flexDirection: 'row',
+      },
+      col: {
+        flexDirection: 'column',
+      },
+      rowReverse: {
+        flexDirection: 'row-reverse',
+      },
+      colReverse: {
+        flexDirection: 'column-reverse',
+      },
+    },
+    justify: {
+      start: {
+        justifyContent: 'flex-start',
+      },
+      end: {
+        justifyContent: 'flex-end',
+      },
+      center: {
+        justifyContent: 'center',
+      },
+      stretch: {
+        justifyContent: 'stretch',
+      },
+      spaceBtw: {
+        justifyContent: 'space-between',
+      },
+      spaceAround: {
+        justifyContent: 'space-around',
+      },
+      spaceEvenly: {
+        justifyContent: 'space-evenly',
+      },
+    },
+
+    align: {
+      start: {
+        alignItems: 'flex-start',
+      },
+      end: {
+        alignItems: 'flex-end',
+      },
+      center: {
+        alignItems: 'center',
+      },
+      stretch: {
+        alignItems: 'stretch',
+      },
+    },
+
+    gap,
+
+    center: {
+      true: {
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+    },
+  },
+});
